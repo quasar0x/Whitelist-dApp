b644eca6447bb0d4aa28d32bd0cbcb2f02828442
Daniel Izevbije <dizevbije94@gmail.com>
2022-11-01 09:20:59 +0100

    first commit

---
+++ my-hardhat-tutorial/contracts/Whitelist.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    // maximum number of addresses which can be whitelisted
+    uint8 public maxWhitelistedAddresses;
+
+    // keep track of number of addresses whitelisted till now
+    uint8 public numAddressesWhitelisted;
+
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        // msg.sender is the address of the user who called this function
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses cant be added, limit reached"
+        );
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
+}
